7495329ef48abffe133e67848c140d63d2fd58f7
EmiFyn <keith.burton@gmail.com>
2026-09-25 09:27:12 +0100

    v0.06

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 
    VERSION is set by build.py. Changing it is what installs a new release: the
    old cache is thrown away and the new files are taken. */
-var VERSION = "5dd1ce75";
+var VERSION = "566bebb2";
 var CACHE = "landowners-" + VERSION;
 
 var SHELL = ["./", "./index.html", "./manifest.webmanifest",
